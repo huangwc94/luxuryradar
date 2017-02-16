a7c82b6a6e4cc9eca440a6795b6858c02f03007d
Weicheng Huang <huangwc94@gmail.com>
2017-02-16 17:29:25 -0500

    what is environ

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def app(environ, start_response):
     data = b"Hello, World!\n"
+
+    print environ
+
+
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
