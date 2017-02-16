eb8ab029e6d0d15b4e15bf3885d5f555b39bf2c1
Weicheng Huang <huangwc94@gmail.com>
2017-02-16 17:52:56 -0500

    add structure

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
+import json
+def analyze(image):
+    id = 483932343
+    return id
+
+def connector():
+    d = {
+        "id": analyze("asdf")
+    }
+    return json.dumps(d)
+
 def app(environ, start_response):
-    data = b"Hello, World!\n"
 
-    print environ
-
-
-    start_response("200 OK", [
-        ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(data)))
-    ])
+    if environ['REQUEST_METHOD'] == 'POST':
+        data = connector()
+        start_response("200 OK", [
+            ("Content-Type", "application/json"),
+            ("Content-Length", str(len(data)))
+        ])
+    elif environ['PATH_INFO'] == "/favicon.ico":
+        with open('favicon.ico', mode='rb') as file:
+            data = file.read()
+        start_response("200 OK", [
+            ("Content-Type", "text/x-icon"),
+            ("Content-Length", str(len(data)))
+        ])
+    else:
+        data = open('index.html').read()
+        start_response("200 OK", [
+            ("Content-Type", "image/html"),
+            ("Content-Length", str(len(data)))
+        ])
     return iter([data])
