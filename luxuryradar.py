dc5b3750cf39c721e6b39c7ababcfb2752655727
Weicheng Huang <huangwc94@gmail.com>
2017-02-17 00:29:08 -0500

    pythonanywhere finially working

--- luxuryradar.py
+++ luxuryradar.py
@@ -1,16 +1,20 @@
 import cv2
 import numpy as np
 import json
-def identify(data):
-    a = np.fromstring(data,np.uint32)
 
+
+def identify(data):
+    a = np.asarray(bytearray(data), dtype=np.uint8)
+    b = cv2.imdecode(a, cv2.IMREAD_UNCHANGED)
 
     return json.dumps({
-        "id":62346,
-        "size":len(data),
-        "version":cv2.__version__
+        "id": 62346,
+        "size": len(b),
+        "version": cv2.__version__,
+        "data": str(b[1])
     })
 
+
 if __name__ == "__main__":
-    f = open("foo.jpg","rb")
+    f = open("foo.jpg", "rb")
     print identify(f.read())
