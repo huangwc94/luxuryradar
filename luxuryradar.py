d515b3165d18a3702f95b0e611be9c032bec68ba
Weicheng Huang <huangwc94@gmail.com>
2017-02-16 23:42:07 -0500

    wergf

--- luxuryradar.py
+++ luxuryradar.py
@@ -2,10 +2,15 @@ import cv2
 import numpy as np
 import json
 def identify(data):
-    a = np.fromstring(data)
+    a = np.fromstring(data,np.uint32)
+
 
     return json.dumps({
-        "id":3333333,
+        "id":62346,
         "size":len(data),
         "version":cv2.__version__
     })
+
+if __name__ == "__main__":
+    f = open("foo.jpg","rb")
+    print identify(f.read())
