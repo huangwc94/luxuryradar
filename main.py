bca38d35e0dbabc3b7f95be22d9d33bdc7c60501
Weicheng Huang <huangwc94@gmail.com>
2017-02-16 22:35:17 -0500

    test

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-
+import cv2
+import numpy as np
+import json
 def identify(data):
+    a = np.fromstring(data)
+
+    return json.dumps({
+        "id":123123123,
+        "version":cv2.__version__
+    })
